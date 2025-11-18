685477e0c0639ef5a6b96882aa24b96a19d6fedd
Mar5 <isaura.rios070@gmail.com>
2025-11-17 19:28:41 -0500

    Limpieza

--- components/index.js
+++
@@ -1,6 +0,0 @@
-// Importar los componentes
-import './header.js';
-import './footer.js';
-
-// Los componentes están ahora disponibles globalmente como <custom-header> y <custom-footer>
-console.log('Componentes cargados correctamente');
